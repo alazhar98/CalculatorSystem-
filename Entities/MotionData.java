70f1287b2dd6afbaca001642b3061dd25f2b9569
alazhar <alazhar9814@gmail.com>
2024-11-16 11:57:02 +0400

    this is class MotionData

--- Entities/MotionData.java
+++ Entities/MotionData.java
@@ -5,4 +5,27 @@ public class MotionData {
     private double acceleration;
     private double time;
     private BaseData baseData;
+
+    public MotionData(double initialVelocity, double acceleration, double time, String description) {
+        this.initialVelocity = initialVelocity;
+        this.acceleration = acceleration;
+        this.time = time;
+        this.baseData = new BaseData(description);
+    }
+    public double getInitialVelocity() {
+        return initialVelocity;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public BaseData getBaseData() {
+        return baseData;
+    }
+
 }
