a89d5106ad7b168140d6b797aa46a0096f266df9
alazhar <alazhar9814@gmail.com>
2024-11-16 12:09:15 +0400

    this is class CircleData

---
+++ Entities/CircleData.java
@@ -0,0 +1,20 @@
+package Entities;
+
+public class CircleData {
+    private double radius;
+    private BaseData baseData;
+
+    public CircleData (double radius , String description){
+        this.radius=radius;
+        this.baseData = new BaseData(description);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public BaseData getBaseData() {
+        return baseData;
+    }
+
+}
