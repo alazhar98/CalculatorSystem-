477939720517142dd7549c2d3fc8b806d20544c4
alazhar <alazhar9814@gmail.com>
2024-11-16 12:04:31 +0400

    this is class InterestData

---
+++ Entities/InterestData.java
@@ -0,0 +1,31 @@
+package Entities;
+
+public class InterestData {
+    private double principal;
+    private double rate;
+    private double time;
+    private PhysicsData physicsData;
+
+    public InterestData(double principal, double rate , double time , PhysicsData physicsData){
+         this.principal=principal;
+         this.rate=rate;
+         this.physicsData=physicsData;
+         this.time=time;
+    }
+    public double getPrincipal() {
+        return principal;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public PhysicsData getPhysicsData() {
+        return physicsData;
+    }
+
+}
